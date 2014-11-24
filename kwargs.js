fa294ffea701d14977c5b2ded3c8c73e5cec5b04
Serkan Yersen <serkanyersen@gmail.com>
2014-11-24 13:21:47 -0800

    Whitespace fixes and shorter regexp

--- kwargs.js
+++ kwargs.js
@@ -18,44 +18,45 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-(function(){
+(function() {
     'use strict';
 
     function kwargs(defaults) {
-        var func = this;
-        var removeComments = new RegExp('(\\/\\*[\\w\\\'\\,\\(\\)\\s\\r\\n\\*]*\\*\\/)|(\\/\\/[\\w\\s\\\'][^\\n\\r]*$)|(<![\\-\\-\\s\\w\\>\\/]*>)', 'gim');
-        var removeWhitespc = new RegExp('\\s+', 'gim');
-        var matchSignature = new RegExp('function.*?\\((.*?)\\)', 'i');
-        // get the argument names from function source
-        var names = func.toString()
+        /*jshint validthis:true */
+        var func = this,
+            removeComments = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/gm,
+            removeWhitespc = /\s+/gm,
+            matchSignature = /^function\s*[^\(]*\(\s*([^\)]*)\)/m,
+            // get the argument names from function source
+            names = func.toString()
                         .replace(removeComments, '')
                         .replace(removeWhitespc, '')
                         .match(matchSignature)[1]
                         .split(',');
 
-        // Check the existance of default, if not create an object
-        if(defaults !== Object(defaults)){
+        // Check the existence of default, if not create an object
+        if (defaults !== Object(defaults)) {
             defaults = {};
         }
 
         return function() {
             var args = Array.prototype.slice.call(arguments),
                 kwargs = args[args.length - 1],
-                name;
+                name, i;
 
             // Check the existance of the kwargs
             if (kwargs === Object(kwargs)) {
                 args.pop();
-            }else{
+            } else {
                 kwargs = {};
             }
 
             // Fill the arguments and apply them
-            for (var i = 0; i < names.length; i++) {
+            for (i = 0; i < names.length; i++) {
                 name = names[i];
                 if (name in kwargs) {
                     args[i] = kwargs[name];
-                }else if(name in defaults && args[i] === undefined){
+                } else if (name in defaults && args[i] === undefined) {
                     args[i] = defaults[name];
                 }
             }
@@ -63,27 +64,31 @@
             return func.apply(this, args);
         };
     }
+
     // As function prototype
     Function.prototype.kwargs = kwargs;
 
-    // as a seperate function for module loaders
-    var out = function(){
-        var args = Array.prototype.slice.call(arguments, 0);
-        var org = args.shift();
+    // as a separate function for module loaders
+    var out = function() {
+        var args = Array.prototype.slice.call(arguments, 0),
+            org = args.shift();
+
         return kwargs.apply(org, args);
     };
 
     // CommonJS module is defined
     if (typeof exports !== 'undefined') {
-        if (typeof module !== 'undefined' && module.exports){
+        if (typeof module !== 'undefined' && module.exports) {
             module.exports = out;
         }
         exports.kwargs = out;
-    }else
+    } else {
         // Register as a named module with AMD.
-        if (typeof define === 'function' && define.amd){
-            define('kwargs', [], function(){ return out; });
-        }else{
+        if (typeof define === 'function' && define.amd) {
+            define('kwargs', [], function() { return out; });
+        } else {
             this.kwargs = out;
         }
+    }
+
 }).call(this);
